3912fa4e915aa8f875bdf345d629dcde7afe33d5
Zdenek ULRYCH <ulrychz@office365.zcu.cz>
2024-05-06 16:34:40 +0200

    cv 12

--- Po1450_SVGAnimace/Models/Obrazec.cs
+++ Po1450_SVGAnimace/Models/Obrazec.cs
@@ -15,5 +15,20 @@ namespace Po1450_SVGAnimace.Models
         public Color Barva { get; set; }
         public int PozX { get; set; }
         public int PozY { get; set;}
+
+        public void PosunObjekt(int krok, int svgWidth, int svgHeight, int vychoziPozice)
+        {
+            PozX += krok;
+            PozY += krok;
+
+            if (PozX >= svgWidth + Math.Abs(vychoziPozice)) 
+            {
+                PozX = vychoziPozice;
+            }
+            if (PozY >= svgHeight + Math.Abs(vychoziPozice))
+            {
+                PozY = vychoziPozice;
+            }
+        }
     }
 }
--- Po1450_SVGAnimace/Pages/SVG.razor.cs
+++ Po1450_SVGAnimace/Pages/SVG.razor.cs
@@ -1,14 +1,21 @@
-﻿using Po1450_SVGAnimace.Models;
+﻿using Newtonsoft.Json;
+using Po1450_SVGAnimace.Models;
 using System.Drawing;
 
 namespace Po1450_SVGAnimace.Pages
 {
     public partial class SVG
     {
+        public SVG() 
+        {
+            Task.Run(NacistNastaveni);
+        }
         public int SvgWidth { get; set; } = 1600;
         public int SvgHeight { get; set; } = 800;
         public int MinRozmer { get; set; } = 20;
         public int MaxRozmer { get; set; } = 100;
+        public bool AnimaceBezi { get; set; } = false;
+        public int AnimaceKrok { get; set; } = 1;
 
         private Random rnd = new Random();
         private int TypObrazcePocet => Enum.GetNames(typeof(Globals.TypObrazce)).Length;
@@ -46,6 +53,65 @@ namespace Po1450_SVGAnimace.Pages
             {
                 SvgObrazceList.Add(obrazec);
             }
+        }
+        private void OdebratObrazec(Microsoft.AspNetCore.Components.Web.MouseEventArgs e)
+        {
+            if (SvgObrazceList.Any())
+            {
+                SvgObrazceList.RemoveAt(SvgObrazceList.Count - 1);
+            }
+        }
+
+      
+        private async Task UlozitNastaveni(Microsoft.AspNetCore.Components.Web.MouseEventArgs e)
+        {
+            string json = JsonConvert.SerializeObject(SvgObrazceList, 
+                new JsonSerializerSettings { TypeNameHandling = TypeNameHandling.Auto, Formatting = Formatting.Indented });
+            await localStorage.SetItemAsync("data", json);
+        }
+
+        private async Task NacistNastaveni()
+        {
+            var json = await localStorage.GetItemAsync<string>("data");
+            if (json != null)
+            {
+                var obrazce = JsonConvert.DeserializeObject<List<Obrazec>>(json, 
+                    new JsonSerializerSettings { TypeNameHandling = TypeNameHandling.Auto, Formatting = Formatting.Indented });
+                if (obrazce != null && obrazce.Any())
+                {
+                    SvgObrazceList.AddRange(obrazce);
+                    StateHasChanged();
+                }
+            }
+            
+        }
+        private async Task SmazatNastaveni()
+        {
+            await localStorage.RemoveItemAsync("data");
+        }
+
+        private void AnimaceSpustit()
+        {
+            AnimaceBezi = true;
+            Task.Run(Posun);
+        }
+
+        private async Task Posun()
+        {
+            do
+            {
+                foreach (var item in SvgObrazceList)
+                {
+                    item.PosunObjekt(AnimaceKrok, SvgWidth, SvgHeight, -MaxRozmer);
+                }
+                StateHasChanged() ;
+                await Task.Delay(10);
+            } while (AnimaceBezi);
+        }
+
+        private void AnimaceZastavit()
+        {
+            AnimaceBezi = false;
         }
     }
 }
